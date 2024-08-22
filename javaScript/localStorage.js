be1abcdcccb09280de07d42388c97ff43b576ee8
webcodeshubham <theshubhamshri@gmail.com>
2024-08-22 23:01:58 +0530

    call bind fetch error handling

--- javaScript/localStorage.js
+++ javaScript/localStorage.js
@@ -1,2 +1,10 @@
 localStorage.setItem("userName", "Kudrat");
 localStorage.setItem("age", "24");
+
+let name = localStorage.getItem("username");
+let age = localStorage.getItem("age");
+console.log(`Name: ${name}, Age: ${age}`);
+
+localStorage.removeItem("age");
+
+localStorage.clear();
